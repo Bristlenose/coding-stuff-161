dd877c302b16d9fff8a17f3691b69c8ecfb2c995
Bristlenose <67295297+Bristlenose@users.noreply.github.com>
2023-04-20 14:52:05 +1200

    .

--- Lab 6/Flags.java
+++ Lab 6/Flags.java
@@ -3,19 +3,24 @@ import java.awt.*;
 
 public class Flags{
    public static void main(String[]args){
+      //creates window
       JFrame frame = new JFrame("Some JPanels"); 
       frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
       frame.setLayout(new FlowLayout());
       
+      //flag 1
       Georgia smiley = new Georgia();
       frame.getContentPane().add (smiley);
       
+      //flag 2
       Chile smiley2 = new Chile();
       frame.getContentPane().add (smiley2);
       
+      //add to window
       frame.getContentPane().add (smiley);
       frame.getContentPane().add (smiley2);
       
+      //show window
       frame.pack();
       frame.setVisible(true);
       
