7acf9ec4c080d144409e0b05d1199a66613204d4
Bristlenose <67295297+Bristlenose@users.noreply.github.com>
2023-05-09 17:02:30 +1200

    .

--- Lab 9/Lab09/Lab09/MyToolsTester.java
+++ Lab 9/Lab09/Lab09/MyToolsTester.java
@@ -4,19 +4,19 @@ public class MyToolsTester{
    //makes new tool
       MyTools tool1 = new MyTools();
       
-      //tools non static calling
-      tool1.readInt();
-      tool1.readDouble();
-      tool1.readToken();
-      tool1.readLine();
-      
-      System.out.println();
-      
-      //tools static calling
-      int usrInputint = MyTools.readInt("Please enter your Age");
-      double usrInputdouble = MyTools.readDouble("Please enter a Double"); 
-      String usrInputtoken = MyTools.readLine("Please enter a Token"); 
-      String usrInputstring = MyTools.readLine("Please enter a String");
+//       //tools non static calling
+//       tool1.readInt();
+//       tool1.readDouble();
+//       tool1.readToken();
+//       tool1.readLine();
+//       
+//       System.out.println();
+//       
+//       //tools static calling
+//       int usrInputint = MyTools.readInt("Please enter your Age");
+//       double usrInputdouble = MyTools.readDouble("Please enter a Double"); 
+//       String usrInputtoken = MyTools.readLine("Please enter a Token"); 
+//       String usrInputstring = MyTools.readLine("Please enter a String");
       
       //random number
       int random = MyTools.randomInRange(3,0);   
